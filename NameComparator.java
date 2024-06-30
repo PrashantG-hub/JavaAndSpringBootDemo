62fc39cd086c4e9ffa61963f5f5c4c437a946fbd
Prashant <125962967+PrashantG-hub@users.noreply.github.com>
2024-06-30 21:55:10 +0530

    Add files via upload

---
+++ NameComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+public class NameComparator implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+// TODO Auto-generated method stub
+        return o1.getEmpName().compareToIgnoreCase(o2.getEmpName());
+    }
+}
